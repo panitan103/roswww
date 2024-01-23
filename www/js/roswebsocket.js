711e54d1c262ee9a8fdf3cd01b1a1896142f2262
panitan103 <panitan103_54@yahoo.com>
2024-01-24 00:21:13 +0700

    add publish example

--- www/js/roswebsocket.js
+++ www/js/roswebsocket.js
@@ -1,21 +1,22 @@
 
-function ros_websocket_conenct(ws){
-    var ros = ws
 
-    ros.on('connection', function() {
-        // document.getElementById("status").innerHTML = "Connected";
-        console.log("Connected");
+var ros = new ROSLIB.Ros({
+    url : 'ws://localhost:9090'
+});
 
-    });
+ros.on('connection', function() {
+    // document.getElementById("status").innerHTML = "Connected";
+    console.log("Connected");
 
-    ros.on('error', function(error) {
-        // document.getElementById("status").innerHTML = "Error";
-        console.log("Error");
-    });
+});
 
-    ros.on('close', function() {
-        // document.getElementById("status").innerHTML = "Closed";
-        console.log("Closed");
-    });
-    return ros
-}
+ros.on('error', function(error) {
+    // document.getElementById("status").innerHTML = "Error";
+    console.log("Error");
+});
+
+ros.on('close', function() {
+    // document.getElementById("status").innerHTML = "Closed";
+    console.log("Closed");
+});
+
--- www/js/subscribe.js
+++ www/js/subscribe.js
@@ -1,53 +1,21 @@
 
-var toggleSwitch = document.getElementById("myToggle");
-var toggle_status= "Off";
-let web_socket=null;
-let ros=null;
-toggleSwitch.addEventListener("change", function() {
-    
-    
-    // Check if the toggle switch is checked (ON) or unchecked (OFF)
-    
-    if (toggleSwitch.checked) {
-        var ws=new ROSLIB.Ros({
-            url : 'ws://localhost:9090'
+var toggleSwitch_sub = document.getElementById("button-sub");
+
+toggleSwitch_sub.addEventListener("click", function() {
+    var topic_input_sub = document.getElementById("topic_input_sub").value;
+    var msg_input_sub = document.getElementById("msg_input_sub").value;
+
+    var topic_message_sub = new ROSLIB.Topic({
+        ros : ros,
+        name : topic_input_sub,
+        messageType : msg_input_sub
         });
-        ros=ros_websocket_conenct(ws)
-        
-        var topic_input = document.getElementById("topic_input").value;
-        var msg_input = document.getElementById("msg_input").value;
 
-        var topic_message = new ROSLIB.Topic({
-            ros : ros,
-            name : topic_input,
-            messageType : msg_input
-            });
 
-        
-        topic_message.subscribe(function(message) {
-        console.log('Received message on ' + topic_message.name + ': ' +  message.data);
-    
-        var a = document.getElementById('textArea');
-        a.textContent = new Date() + ' : Received message on ' + topic_message.name + ': ' + message.data;
-        });
-        // const uri = 'ws://localhost:9090/';
-        // ws=new WebSocket(uri);
+    topic_message_sub.subscribe(function(message) {
+    console.log('Received message on ' + topic_message_sub.name + ': ' +  message.data);
 
-        // msg=openWebSocket(ws,subscribe)
-        // console.log(msg.data)
-    
-        
-        
-        
-    //     // Perform actions when the switch is ON
-    } else {
-        ros.close()
-     
-    //     //console.log("Toggle switch is OFF");
-    //     toggle_status= "Off";
-    //     ros.close
-    //     // closeWebSocket(socket)
-    //     // Perform actions when the switch is OFF
-    }
-    
+    var a = document.getElementById('textArea');
+    a.textContent = new Date() + ' : Received message on ' + topic_message_sub.name + ': ' + JSON.stringify(message);
+    })
 });
